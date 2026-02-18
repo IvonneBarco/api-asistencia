5ba20f0649580ebd8180efddbcfac4afd9000054
Ivonne C. Barco <ivonne.barco@loganitsolutions.com>
2026-02-18 18:27:21 -0500

    fix(leaderboard): Re-enable JwtAuthGuard in LeaderboardController

--- src/leaderboard/leaderboard.controller.ts
+++ src/leaderboard/leaderboard.controller.ts
@@ -1,8 +1,9 @@
-import { Controller, Get, Request } from '@nestjs/common';
+import { Controller, Get, UseGuards, Request } from '@nestjs/common';
 import { LeaderboardService } from './leaderboard.service';
+import { JwtAuthGuard } from '../auth/guards/jwt-auth.guard';
 
 @Controller('leaderboard')
-// @UseGuards(JwtAuthGuard)
+@UseGuards(JwtAuthGuard)
 export class LeaderboardController {
   constructor(private leaderboardService: LeaderboardService) {}
 
